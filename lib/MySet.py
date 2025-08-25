2dab531a7ce5473c68b9f8915268160dacf2254f
Your Name <your-email@example.com>
2025-08-25 14:31:26 +0300

    add myset.py

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,37 @@
 class MySet:
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+            
+    def has(self, value):
+     return value in self.dictionary
+     
+    def add(self, value):
+     self.dictionary[value] = True
+     return self
+ 
+    def delete(self, value):
+     self.dictionary.pop(value, None)
+     return self
+ 
+    def size(self):
+     return len(self.dictionary)
+ 
+    def clear(self):
+        self.dictionary.clear()
+        return self
 
-    pass
+    def __str__(self):
+        set_list = [str(k) for k in self.dictionary]
+        return f'MySet: {{{", ".join(set_list)}}}'
+
+s = MySet([1,2,3,3])
+print(s)       
+s.add(4)
+print(s)       
+s.delete(2)
+print(s.has(2))
+print(s.size()) 
+s.clear()
+print(s)      
